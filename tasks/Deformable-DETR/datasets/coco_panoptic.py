b179e152181fbc06e229bcebb98c662b53db2542
ishvi-m <ishvimathai@gmail.com>
2024-05-17 18:25:27 -0700

    added print statement

--- tasks/Deformable-DETR/datasets/coco_panoptic.py
+++ tasks/Deformable-DETR/datasets/coco_panoptic.py
@@ -103,5 +103,6 @@ def build(image_set, args):
 
     dataset = CocoPanoptic(img_folder_path, ann_folder, ann_file,
                            transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    print(dataset)
 
     return dataset
